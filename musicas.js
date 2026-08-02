5c0420f25e561cef5f54ac49f2e72d03a4d5f8b2
tarcisiogb <tarcisiogb@gmail.com>
2026-08-02 08:12:11 -0300

    Add files via upload

---
+++ musicas.js
@@ -0,0 +1,589 @@
+// Gerado automaticamente por gerar_musicas.py
+// Titulos e dedicatorias podem ser editados aqui. Os temas ficam em temas.py.
+window.TEMAS_ORDEM = ["Luz e céu", "Horas do dia", "Estações e dias", "Domingos", "Pedras, cores e flores", "Texturas e ornamentos", "Formas e enigmas", "Mundo afora", "Inspirações", "Clássicos e homenagens", "Valsas", "Pista e groove", "Afetos e dedicatórias"];
+window.MUSICAS = [
+  {
+    "titulo": "Cintilante",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Luz e céu",
+    "capa": "capas/CINTILANTE.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/CINTILANTE.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Heaven",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Luz e céu",
+    "capa": "capas/HEAVEN.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/HEAVEN.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Iluminando",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Luz e céu",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/ILUMINANDO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Lumina",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Luz e céu",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/LUMINA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Reflexos",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Luz e céu",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/REFLEXOS.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Sublime",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Luz e céu",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/SUBLIME.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Universo",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Luz e céu",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/UNIVERSO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Amanhecer",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Horas do dia",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/AMANHECER.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Brisa",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Horas do dia",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/BRISA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Fantasia Crepusculo",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Horas do dia",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/FANTASIA_CREPUSCULO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Madrugada",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Horas do dia",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/MADRUGADA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Noite",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Horas do dia",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/NOITE.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Orvalho",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Horas do dia",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/ORVALHO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Penumbra",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Horas do dia",
+    "capa": "capas/PENUMBRA.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/PENUMBRA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Outono",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Estações e dias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/OUTONO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Printemps",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Estações e dias",
+    "capa": "capas/PRINTEMPS.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/PRINTEMPS.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Sabado",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Estações e dias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/SABADO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Summer",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Estações e dias",
+    "capa": "capas/SUMMER.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/SUMMER.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Sunny",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Estações e dias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/SUNNY.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Verao",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Estações e dias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/VERAO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Domingo Balad16",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Domingos",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/DOMINGO_BALAD16.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Domingo Pop",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Domingos",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/DOMINGO_POP.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Domingosolo",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Domingos",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/DOMINGOSOLO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Ambar",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pedras, cores e flores",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/AMBAR.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Azul",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pedras, cores e flores",
+    "capa": "capas/AZUL.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/AZUL.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Jade",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pedras, cores e flores",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/JADE.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Lilas",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pedras, cores e flores",
+    "capa": "capas/LILAS.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/LILAS.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Orquideas",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pedras, cores e flores",
+    "capa": "capas/ORQUIDEAS.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/ORQUIDEAS.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Perola",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pedras, cores e flores",
+    "capa": "capas/PEROLA.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/PEROLA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Rubi",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pedras, cores e flores",
+    "capa": "capas/RUBI.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/RUBI.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Filigrana",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Texturas e ornamentos",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/FILIGRANA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Mosaico",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Texturas e ornamentos",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/MOSAICO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Seda",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Texturas e ornamentos",
+    "capa": "capas/SEDA.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/SEDA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Veludo",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Texturas e ornamentos",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/VELUDO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Vintage",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Texturas e ornamentos",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/VINTAGE.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Abstrato",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Formas e enigmas",
+    "capa": "capas/ABSTRATO.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/ABSTRATO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Enigma",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Formas e enigmas",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/ENIGMA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Fractais",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Formas e enigmas",
+    "capa": "capas/FRACTAIS.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/FRACTAIS.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Labirinto",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Formas e enigmas",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/LABIRINTO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Argentina",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Mundo afora",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/ARGENTINA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Cigana",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Mundo afora",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/CIGANA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Cosita",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Mundo afora",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/COSITA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Espanola",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Mundo afora",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/ESPANOLA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Ibiza Fake",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Mundo afora",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/IBIZA_FAKE.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Libya",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Mundo afora",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/LIBYA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Adagio",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Clássicos e homenagens",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/ADAGIO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Bach",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Clássicos e homenagens",
+    "capa": "capas/BACH.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/BACH.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Intermezzo",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Clássicos e homenagens",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/INTERMEZZO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Majestosa",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Clássicos e homenagens",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/MAJESTOSA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Mozariana",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Clássicos e homenagens",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/MOZARIANA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Piano",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Clássicos e homenagens",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/PIANO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Suite O Tempo 2021",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Clássicos e homenagens",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/SUITE_O_TEMPO_2021.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Tangata",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Clássicos e homenagens",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/TANGATA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Valsa Maior",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Valsas",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/VALSA_MAIOR.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Valsinha",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Valsas",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/VALSINHA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Acelerada",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pista e groove",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/ACELERADA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Blues",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pista e groove",
+    "capa": "capas/BLUES.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/BLUES.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Funkiest House",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pista e groove",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/FUNKIEST_HOUSE.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Golden Blues",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pista e groove",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/GOLDEN_BLUES.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Movie",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pista e groove",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/MOVIE.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Soft",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Pista e groove",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/SOFT.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Companheira",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Afetos e dedicatórias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/COMPANHEIRA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Doce Mel",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Afetos e dedicatórias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/DOCE_MEL.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Libertaria",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Afetos e dedicatórias",
+    "capa": "capas/LIBERTARIA.PNG",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/LIBERTARIA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Meu Cantinho",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Afetos e dedicatórias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/MEU_CANTINHO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Seu Juca",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Afetos e dedicatórias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/SEU_JUCA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Suave",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Afetos e dedicatórias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/SUAVE.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Vitoria",
+    "autor": "Tarcísio Buzelin",
+    "tema": "Afetos e dedicatórias",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/VITORIA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Go Nogo",
+    "autor": "Tarcísio Buzelin",
+    "tema": "A organizar",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/GO-NOGO.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "InspiraçÃO Hotel Calif",
+    "autor": "Tarcísio Buzelin",
+    "tema": "A organizar",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/INSPIRAC%CC%A7A%CC%83O_HOTEL_CALIF.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "InspiraçÃO Souvenir Da China",
+    "autor": "Tarcísio Buzelin",
+    "tema": "A organizar",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/INSPIRAC%CC%A7A%CC%83O_SOUVENIR_DA_CHINA.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Raid",
+    "autor": "Tarcísio Buzelin",
+    "tema": "A organizar",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/RAID.mp3",
+    "dedicatoria": ""
+  },
+  {
+    "titulo": "Tosw",
+    "autor": "Tarcísio Buzelin",
+    "tema": "A organizar",
+    "capa": "",
+    "audio": "https://pub-5fc5d1d8ff504c9faac869f244dd9d2f.r2.dev/musicas/TOSW.mp3",
+    "dedicatoria": ""
+  }
+];
